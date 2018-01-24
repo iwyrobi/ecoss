43c47cb3f9aa73c924ecf7ec99e1bcc6e9489bc4
iwyrobi <iwyrobi@gmail.com>
2018-01-24 13:57:04 +0800

    commit

--- webadmin/product-save.php
+++ webadmin/product-save.php
@@ -3,20 +3,19 @@ require("connect.php");
 $postdata = file_get_contents("php://input");
 
 if (isset($postdata) && !empty($postdata)){
-   
-    $request  = json_decode($postdata);
-    var_dump($postdata);
-    $name = $_POST['Name'];
-    $location = $_POST['Location'];
-    $category = $request['Category'];
-    $photo = $request['Photo'];
-    $description = $request['Description'];
-    $email = $request['Email'];
-    $contact = $request['Contact'];
-    $flag = $request['Flag'];
+     
+    $request  = json_decode($postdata);    
+    $name = $request->Name;
+    $location = $request->Location;
+    $category = $request->Category;
+    $photo = $request->Photo;
+    $description = $request->Description;
+    $email = mysqli_real_escape_string($conn,$request->Email);
+    $contact = $request->Contact;
+    $flag = $request->Flag;
 
-    $sql = "INSERT INTO tenant (name,location,category,photo,description, email,contact,flag)
-            VALUES ($name, $location, $category, $photo, $description, $email,$contact,$flag)";
+    $sql = "INSERT INTO tenant (name,location,category,photo,description,email,contact,flag)
+            VALUES ('$name', $location, $category, '$photo', '$description','$email','$contact',$flag)";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
---
+++ webadmin/script/myscript.js
@@ -0,0 +1,96 @@
+var app = angular.module('myApp', ['ui.bootstrap']);
+
+app.filter('beginning_data', function() {
+    return function(input, begin) {
+        if (input) {
+            begin = +begin;
+            return input.slice(begin);
+        }
+        return [];
+    }
+});
+
+app.controller('TenantController', function($scope, $http,$timeout) {
+	$scope.names;
+	$scope.loc;
+	$scope.cat;
+	$scope.Flag = 0;
+	$scope.messageSuccess;
+	// function get list of tenant
+
+
+	$('.alert').hide();
+
+	$scope.getTenant = function(){
+		$http.get("product.php?list")
+		.then(function (response) {
+            $scope.file = response.data.records;
+            $scope.current_grid = 1;
+            $scope.data_limit = 2;
+            $scope.filter_data = $scope.file.length;
+            $scope.entire_user = $scope.file.length;
+        
+        });
+	};
+
+    $scope.page_position = function(page_number) {
+        $scope.current_grid = page_number;
+    };
+    $scope.filter = function() {
+        $timeout(function() {
+            $scope.filter_data = $scope.searched.length;
+        }, 20);
+    };
+    $scope.sort_with = function(base) {
+        $scope.base = base;
+        $scope.reverse = !$scope.reverse;
+    };
+
+	// function delete tenant
+	$scope.deleteTenant = function(){
+		$http.get("product.php")
+		.then(function (response) {$scope.names = response.data.records;});
+	};
+
+	//function save tenant	
+	$scope.saveTenant = function(){
+		$http.post('product-save.php',{Name: $scope.Name,
+				Location: $scope.Location,
+				Category: $scope.Category,
+				Photo: $scope.Photo,
+				Description: $scope.Description,
+				Email: $scope.Email,
+				Contact: $scope.Contact,
+				Flag: $scope.Flag})
+		.then(function (response) {
+			  $scope.getTenant();
+			  $scope.messageSuccess= response.data;
+			  $('.alert').show();
+			  $scope.emptyForm();
+		      //console.log(response.data,response.status); 
+			  });
+	};
+
+	$scope.getLocation = function(){
+		$http.get("product.php?loc")
+		.then(function (response) {$scope.loc = response.data.records;});
+	};
+
+	$scope.getCategory = function(){
+		$http.get("product.php?cat")
+		.then(function (response) {$scope.cat = response.data.records;});
+	};
+
+	$scope.emptyForm = function(){
+		 		$scope.Name = "";
+				$scope.Location="";
+				$scope.Category="";
+				$scope.Photo="";
+				$scope.Description="";
+				$scope.Email="";
+				$scope.Contact="";
+				$scope.Flag="";
+	}
+	
+});
+
