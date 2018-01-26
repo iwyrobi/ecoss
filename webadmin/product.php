bad25e6c7f396839fb6c334efcd81ca5754437d3
iwyrobi <iwyrobi@gmail.com>
2018-01-26 12:32:50 +0800

    commit

--- webadmin/product.php
+++ webadmin/product.php
@@ -1,41 +1,7 @@
 <?php
-//header("Access-Control-Allow-Origin: *");
-//header("Content-Type: application/json; charset=UTF-8");
 
-//require "connect.php";
-
-/*
-$conn = connect();
-
-$tenant = array();
-
-$sql = "select * from tenant";
-
-if ($result = mysqli_query($conn,$sql)){
-    $count = mysqli_num_row($result);
-
-    $cr =0;
-    while($row = mysqli_fetch_assoc($result)){
-        $tenant[$cr]['id'] = $row['id'];
-        $tenant[$cr]['name'] = $row['name'];
-        $tenant[$cr]['location'] = $row['location'];
-        $tenant[$cr]['category'] = $row['category'];
-        $tenant[$cr]['photo'] = $row['photo'];
-        $tenant[$cr]['description'] = $row['description'];
-        $tenant[$cr]['email'] = $row['email'];
-        $tenant[$cr]['contact'] = $row['contact'];
-        $tenant[$cr]['flag'] =  $row['flag'];
-        $cr++;
-    }
-
-}
-$json = json_encode($tenant);
-exit;
-*/
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-
-//$conn = new mysqli("localhost", "root", "", "ecoss");
 
 require("connect.php");
 
